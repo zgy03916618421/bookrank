6051e201f3c5c4aff3bcbba102d02d425c465891
zgy03916618421 <576428698@qq.com>
2016-09-14 16:51:41 +0800

    200.2

--- config/productionENV.sh
+++ config/productionENV.sh
@@ -1,6 +1,6 @@
 export BS_MONGODBNAME=bookface
 export BS_MONGOPORT=27017
-export BS_MONGOHOST=192.168.200.22
+export BS_MONGOHOST=192.168.200.2
 export BS_MONGOUSER=bookcover
 export BS_MONGOPASS=5pmKRaVA
 
